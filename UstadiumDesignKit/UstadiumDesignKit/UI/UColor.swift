8ade0482f4d63b2f8326be0bbc9e8a0915580080
noahbino <noahiarrobino@yahoo.com>
2025-11-24 18:51:43 -0500

    some updates

---
+++ UstadiumDesignKit/UstadiumDesignKit/UI/UColor.swift
@@ -0,0 +1,14 @@
+//
+//  UColor.swift
+//  predictor
+//
+//  Created by Noah Iarrobino on 10/30/25.
+//
+
+import SwiftUI
+
+struct UColor {
+    static let primary = Color("PrimaryColor")
+    static let secondary = Color("SecondaryColor")
+    static let background = Color("BackgroundColor")
+}
---
+++ UstadiumDesignKit/UstadiumDesignKit/UI/UTextField.swift
@@ -0,0 +1,8 @@
+//
+//  UTextField.swift
+//  predictor
+//
+//  Created by Noah Iarrobino on 10/30/25.
+//
+
+import Foundation

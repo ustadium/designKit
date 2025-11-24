a9c3a41d6a2879997d8d596e9e28ad8f94b5bc36
noahbino <noahiarrobino@yahoo.com>
2025-11-24 17:08:16 -0500

    initial commit

---
+++ UDS/UDS/UDS.swift
@@ -0,0 +1,9 @@
+//
+//  UDS.swift
+//  UDS
+//
+//  Created by Noah Iarrobino on 11/24/25.
+//
+
+import Foundation
+
---
+++ UDS/UDSTests/UDSTests.swift
@@ -0,0 +1,17 @@
+//
+//  UDSTests.swift
+//  UDSTests
+//
+//  Created by Noah Iarrobino on 11/24/25.
+//
+
+import Testing
+@testable import UDS
+
+struct UDSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
